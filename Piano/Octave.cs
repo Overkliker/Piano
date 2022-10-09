30bc6b8fa4e68cc3c5c890d5b4d3d0f3957a9776
Danila <danilahreno@gmail.com>
2022-10-10 00:07:02 +0300

    Добавьте файлы проекта.

---
+++ Piano/Octave.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Piano
+{
+    public class Octave
+    {
+        static int soundTime = 500; 
+        public int oct = 0;
+
+        public Dictionary<string, int> f_oct = new Dictionary<string, int> {
+            ["q"] = 130, ["w"] = 138,
+            ["e"] = 147, ["r"] = 155,
+            ["t"] = 164, ["y"] = 174,
+            ["u"] = 185, ["i"] = 196,
+            ["o"] = 207, ["p"] = 220,
+            ["f"] = 233, ["h"] = 247 };
+        public Dictionary<string, int> s_oct = new Dictionary<string, int>();
+        public Dictionary<string, int> fd_oct = new Dictionary<string, int>();
+        public Dictionary<string, int> fo_oct = new Dictionary<string, int>();
+        public Dictionary<string, int> fi_oct = new Dictionary<string, int>();
+        public Dictionary<string, int> sx_oct = new Dictionary<string, int>() {
+            ["q"] = 4186, ["w"] = 4434,
+            ["e"] = 4698, ["r"] = 4978, ["t"] = 5274 };
+
+        public void upOct()
+        {
+            foreach (var i in f_oct)
+            {
+                int arc = i.Value * 2;
+                int arc2 = arc * 2;
+                int arc3 = arc2 * 2;
+                int arc4 = arc3 * 2;
+
+                s_oct.Add(i.Key, arc);
+                fd_oct.Add(i.Key, arc2);
+                fo_oct.Add(i.Key, arc3);
+                fi_oct.Add(i.Key, arc4);
+
+            }
+        }
+
+        public void setOctave(int num)
+        {
+            oct = num;
+            
+        }
+
+        public void sound(string key)
+        {
+            switch (oct)
+            {
+                case 0:
+                    System.Console.Beep(f_oct[key], soundTime);
+                    break;
+                case 1:
+                    SystemSound.Beep();
+                    break;
+
+                case 2:
+                    Console.Beep(fd_oct[key], soundTime);
+                    break;
+
+                case 3:
+                    Console.Beep(fo_oct[key], soundTime);
+                    break;
+
+                case 4:
+                    Console.Beep(fi_oct[key], soundTime);
+                    break;
+
+                case 5:
+                    Console.Beep(sx_oct[key], soundTime);
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ Piano/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Media;
+
+namespace Piano
+{
+    public class Programm
+    {
+        static void Main(string[] argrs)
+        {
+            bool flag = false;
+            Octave piano = new Octave();
+            piano.upOct();
+            
+
+            while (!flag)
+            {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Q:
+                            piano.sound("q");
+                            break;
+
+                        case ConsoleKey.W:
+                            piano.sound("w");
+                            break;
+
+                        case ConsoleKey.E:
+                            piano.sound("e");
+                            break;
+
+                        case ConsoleKey.R:
+                            piano.sound("r");
+                            break;
+
+                        case ConsoleKey.T:
+                            piano.sound("t");
+                            break;
+
+                        case ConsoleKey.Y:
+                            piano.sound("y");
+                            break;
+
+                        case ConsoleKey.U:
+                            piano.sound("u");
+                            break;
+
+                        case ConsoleKey.I:
+                            piano.sound("i");
+                            break;
+
+                        case ConsoleKey.O:
+                            piano.sound("o");
+                            break;
+
+                        case ConsoleKey.P:
+                            piano.sound("p");
+                            break;
+
+                        case ConsoleKey.F:
+                            piano.sound("f");
+                            break;
+
+                        case ConsoleKey.H:
+                            piano.sound("h");
+                            break;
+                    }
+
+
+
+                    /////////// Смена октавы //////////
+                    if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.F1)
+                    {
+                        piano.setOctave(0);
+                        Console.WriteLine("gg");
+                    }
+                    else if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.F2)
+                    {
+                        piano.setOctave(1);
+                    }
+                    else if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.F3)
+                    {
+                        piano.setOctave(2);
+                    }
+                    else if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.F4)
+                    {
+                        piano.setOctave(3);
+                    }
+                    else if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.F5)
+                    {
+                        piano.setOctave(4);
+                    }
+                    else if (key.Modifiers.HasFlag(ConsoleModifiers.Control) && key.Key == ConsoleKey.F6)
+                    {
+                        piano.setOctave(5);
+                    }
+                }
+                
+            }
+        }
+    
+}
